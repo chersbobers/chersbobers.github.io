4d25ad26e93f9ae1a9c99a32b4393339331ce4bd
Charlie <chazington7@gmail.com>
2026-02-18 14:44:17 +1300

    test

--- posts.js
+++ posts.js
@@ -1,4 +1,4 @@
-const POSTS = [
+window.POSTS = [
     { date: "2026-16-02", title: "first", url: "posts/first.html" },
     { date: "2026-16-02", title: "booly info", url: "posts/boolyinfo.html" },
     { date: "2026-18-02", title: "yawdl", url: "posts/yawdl.html" },
