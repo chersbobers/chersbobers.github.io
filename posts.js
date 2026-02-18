24d75e43ecb87de5dcfa5f641b7be4d4b802f0b7
Charlie <chazington7@gmail.com>
2026-02-18 15:32:16 +1300

    Ive done it

--- posts.js
+++ posts.js
@@ -1,5 +1,5 @@
 window.POSTS = [
-    { date: "2026-16-02", title: "first", url: "posts/first.html" },
-    { date: "2026-16-02", title: "booly info", url: "posts/boolyinfo.html" },
-    { date: "2026-18-02", title: "yawdl", url: "posts/yawdl.html" },
-]
+    { date: "2026-18-02", title: "yawdl", url: "posts/yawdl" },
+    { date: "2026-16-02", title: "booly info", url: "posts/boolyinfo" },
+    { date: "2026-16-02", title: "first", url: "posts/first" }
+];
